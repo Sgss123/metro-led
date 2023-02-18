f1919a47f0ac6c87c82f4f1546c9aee0fafdb2bf
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2023-02-18 16:22:46 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # metro-led
 LED走字屏模拟
+
+## 关于此项目
+
+此项目通过使用 HTML CSS 来模拟
---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+function showNew() {
+    var text = document.getElementById("basic-addon1");
+    var color = document.getElementById("basic-addon2");
+    document.getElementsById("marquee").innerHTML = text;
+    document.getElementsById("marquee").style.color = color;
+}
